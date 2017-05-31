5f3c3e287cc8e832862cfbbcc2eba9c5a26e4d42
Stevenljl <2234064569@qq.com>
2017-05-31 14:41:03 +0800

    add html

--- src/main/java/com/delivery/Application.java
+++ src/main/java/com/delivery/Application.java
@@ -14,4 +14,8 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
--- src/main/resources/static/js/signin.js
+++ src/main/resources/static/js/signin.js
@@ -1,6 +1,9 @@
 /**
  * Created by 22340 on 2016/11/27.
  */
+
+var GLOBAL = {};
+
 window.onload = function () {
     document.body.style.fontSize = screen.width * 0.01 + "px";
     document.body.style.display = "block";
@@ -13,19 +16,22 @@ $(document).ready(function () {
 });
 
 function sign() {
-    var username = $("[name='username']").val();
+    var uid = $("[name='uid']").val();
     var password = $("[name='password']").val();
-    if (checkForm(username, password) == true) {
+    if (checkForm(uid, password) === true) {
         $.ajax({
             type: "post",
-            url: "/signin",             //向springboot请求数据的url
-            data: {"username": username, "password": password},
+            url: "/tokens",             //向springboot请求数据的url
+            data: {"username": uid, "password": password},
             success: function (json) {
                 $("#loginButton").val("正在登陆");
                 showLoading();
                 setTimeout(function () {
-                    if (json.message == ("success")) {
-                        console.log(json.content)
+                    if (json.message === ("success")) {
+                        console.log(json);
+
+                        GLOBAL.token = json.response.data.token;
+                        // todo
                         location = json.content;        //跳转
                     } else {
                         $("#loginButton").val("登陆失败请重试");
@@ -39,10 +45,10 @@ function sign() {
 
 function checkForm(username, password) {
     var ifInput = true;
-    if (username == "") {
+    if (username === "") {
         ifInput = false;
         $("#loginButton").val("请输入账户名");
-    } else if (password == "") {
+    } else if (password === "") {
         ifInput = false;
         $("#loginButton").val("请输入密码");
     }
