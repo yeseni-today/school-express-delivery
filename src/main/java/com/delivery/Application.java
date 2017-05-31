74bb0995b01c71f0a694a158998dd50f50da69bf
Ticknick <ticknick.hou@gmail.com>
2017-05-31 14:18:10 +0800

    server database

--- src/main/java/com/delivery/Application.java
+++ src/main/java/com/delivery/Application.java
@@ -1,17 +1,17 @@
-//package com.delivery;
-//
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-//import org.springframework.context.annotation.ComponentScan;
-//
-//@SpringBootApplication
-//@ComponentScan("com")
-//public class Application {
-//    //lx5548
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//}
+package com.delivery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com")
+public class Application {
+    //lx5548
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
