1b3b95c76f08d7fcce38f908409bba0cab0cb8ca
Stevenljl <2234064569@qq.com>
2017-05-31 19:40:15 +0800

    ll

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -2,8 +2,7 @@
  * Created by 22340 on 2017/5/31.
  */
 $(document).ready(function () {
-    var $table = $("#reviewList");
-        // reviewList.find("tbody");tbody
+    var $table = $("#reviewList").find("tbody");
     var tablecolor = 'myTable-operation-info';
     // alert(GLOBAL.token);
 
@@ -12,10 +11,7 @@ $(document).ready(function () {
         type: "get",
         data: {"token": getCookie("token"), "state": 0},
         success: function (result) {
-            var tr = $table.find("tr").remove();
-
-            var items = result.data;
-
+            $table.find("tr").remove();
             //加载特效
             var _display = function (item) {
                 var itemhtml ="<tr style='display: none' id='tr" + item.id + "'>" +
@@ -28,15 +24,10 @@ $(document).ready(function () {
                 $table.append(itemhtml);
                 console.log("display:"+itemhtml)
             };
-
-            // for(var  i = 0;i < items.length;i++){
-            //     _display(items[i])
-            // }
-
             var _afterdisplay = function (item) {
                 $("#tr" + item.id).fadeIn(500);
             };
-            beautifyDisplay(_display, _afterdisplay, items, "reviewsList");
+            beautifyDisplay(_display, _afterdisplay, result.data, "reviewsList");
         },
         error: function () {
             alert("ajax请求发送失败");
