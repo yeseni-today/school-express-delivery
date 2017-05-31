e8b01fb250870aa1d66f10ce9a181f89d0ae8680
Stevenljl <2234064569@qq.com>
2017-05-31 22:14:48 +0800

    add html

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         type: "get",
         data: {"token": getCookie("token"), "state": 0},
         success: function (result) {
-            $table.find("tr").remove();
+            // $table.find("tr").remove();
             //加载特效
             var _display = function (item) {
                 var itemhtml ="<tr style='display: none' id='tr" + item.id + "'>" +
@@ -19,7 +19,7 @@ $(document).ready(function () {
                     "<td>" + item.user.name + "</td>" +
                     "<td>" + item.time + "</td>" +
                     "<td class='myTable-operation-info icon-search' "+
-                    " onclick=\"openPop_select(\'" + item.id + "\',\'" + item.user.name + "\',\'icon-search\')\"></td>" +
+                    " onclick=\"openPop_review(\'" + item + "\')\"></td>" +
                     "</tr>";
                 $table.append(itemhtml);
                 console.log("display:"+itemhtml)
@@ -33,5 +33,42 @@ $(document).ready(function () {
             alert("ajax请求发送失败");
         }
     })
-
 });
+
+
+function openPop_review(review) {
+    $(".pop li").css({"min-height": "3em", "line-height": "3em"});  //todo 弹出窗口样式
+    openPop();
+    // $.ajax({
+    //     url: "/query/itemInfo",
+    //     type: "post",
+    //     data: {"itemCode": itemCode},
+    //     success: function (result) {
+    //         if (result.message == "success") {
+    //             openPop();
+    //             var item = result.content.itemEntity;
+    //             document.getElementsByName("itemCode")[1].value = item.itemCode;
+    //             document.getElementsByName("itemName")[1].value = item.itemName;
+    //             //命名冲突
+    //             // $("[name='itemCode']").val(item.itemCode);
+    //             // $("[name='itemName']").val(item.itemName);
+    //             $("[name='itemSpec']").val(item.itemSpec);
+    //             $("[name='itemCount']").val(item.itemCount);
+    //             $("[name='itemPrice']").val(item.itemPrice);
+    //             $("[name='itemIntroduce']").val(item.itemIntroduce);
+    //             $("[name='itemBorrowTimelimit']").val(item.itemBorrowTimelimit);
+    //             $("[name='itemState']").val(item.itemState);
+    //             $("[name='itemExamine']").val(item.itemExamine);
+    //             $("[name='itemRemind']").val(item.itemRemind);
+    //             $("[name='itemCategory']").val(item.categoryEntity.categoryName);
+    //             $("[name='companyName']").val(item.companyEntity.companyName);
+    //             $("[name='itemSlot']").val(result.content.slot);
+    //         } else {
+    //             alert("查询详情出错");
+    //         }
+    //     },
+    //     error: function () {
+    //         alert("ajax请求发送失败");
+    //     }
+    // });
+}
