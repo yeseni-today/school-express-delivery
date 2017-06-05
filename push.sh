30bf31b76d304a7c74c3d6650031fae8788aef74
Stevenljl <2234064569@qq.com>
2017-06-05 22:28:24 +0800

    a

--- push.sh
+++ push.sh
@@ -2,5 +2,12 @@
 git add .
 git commit -m a
 git push
+
+if [ $? -eq 0]
+then echo ok
+else echo "git push eror, could you poll to try"
+	exit 1
+fi
+
 echo push ok
 ssh root@ticknick.me ./sed_deploy
